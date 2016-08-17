91fc97fc997812ce97ce04d348462a1044bfd1c3
lilaizhen <li_laizhen@wentianxia.net>
2016-08-17 14:07:53 +0800

    添加注释

---
+++ src/main/java/com/springkafka/app1/S1pKafkaApplication.java
@@ -0,0 +1,48 @@
+package com.springkafka.app1;
+
+import com.springkafka.CommonConfiguration;
+import com.springkafka.ConfigProperties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.KafkaTemplate;
+
+/**
+ *
+ */
+@SpringBootApplication
+@Import({ CommonConfiguration.class, ConfigProperties.class })
+public class S1pKafkaApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(S1pKafkaApplication.class)
+			.web(false)
+			.run(args);
+		TestBean testBean = context.getBean(TestBean.class);
+		testBean.send("foo");
+	}
+
+	@Bean
+	public TestBean test() {
+		return new TestBean();
+	}
+
+	public static class TestBean {
+
+		@Autowired
+		private ConfigProperties configProperties;
+
+		@Autowired
+		private KafkaTemplate<String, String> template;
+
+		public void send(String foo) {
+			this.template.send(this.configProperties.getTopic(), foo);
+		}
+
+	}
+
+}
---
+++ src/main/java/com/springkafka/app5/S1pKafkaApplication.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.springkafka.app5;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import com.springkafka.Foo;
+import com.springkafka.ConfigProperties;
+import com.springkafka.JsonConfiguration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+/**
+ * @author Gary Russell
+ *
+ */
+@SpringBootApplication
+@Import({ JsonConfiguration.class, ConfigProperties.class })
+@EnableKafka
+public class S1pKafkaApplication {
+
+	public static void main(String[] args) throws Exception {
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(S1pKafkaApplication.class)
+			.web(false)
+			.run(args);
+		TestBean testBean = context.getBean(TestBean.class);
+		testBean.send(new Foo("foo", "bar"));
+		context.getBean(Listener.class).latch.await(60, TimeUnit.SECONDS);
+		context.close();
+	}
+
+	@Bean
+	public TestBean test() {
+		return new TestBean();
+	}
+
+	@Bean
+	public Listener listener() {
+		return new Listener();
+	}
+
+	public static class TestBean {
+
+		@Autowired
+		private ConfigProperties configProperties;
+
+		@Autowired
+		private KafkaTemplate<String, Foo> template;
+
+		public void send(Foo foo) {
+			this.template.send(this.configProperties.getFooTopic(), foo);
+		}
+
+	}
+
+	public static class Listener {
+
+		private final CountDownLatch latch = new CountDownLatch(1);
+
+		@KafkaListener(topics = "${kafka.fooTopic}")
+		public void listen(Foo foo) {
+			System.out.println("Received: " + foo);
+			this.latch.countDown();
+		}
+
+	}
+
+}
