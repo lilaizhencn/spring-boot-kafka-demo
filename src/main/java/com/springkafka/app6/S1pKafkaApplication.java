64d1b82cddb4043b0a747653996d3d7dc6e83062
lilaizhen <lilaizhen@12lian.com>
2017-11-21 11:41:43 +0800

    test spring kafka2.0version  it need spring 5.0 so,update spring boot to 2.0M

--- src/main/java/com/springkafka/app6/S1pKafkaApplication.java
+++ src/main/java/com/springkafka/app6/S1pKafkaApplication.java
@@ -9,6 +9,7 @@ import com.springkafka.CommonConfiguration;
 import com.springkafka.ConfigProperties;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -29,7 +30,7 @@ public class S1pKafkaApplication {
 
 	public static void main(String[] args) throws Exception {
 		ConfigurableApplicationContext context = new SpringApplicationBuilder(S1pKafkaApplication.class)
-			.web(false)
+			.web(WebApplicationType.NONE)
 			.run(args);
 		TestBean testBean = context.getBean(TestBean.class);
 		testBean.send(new GenericMessage<>(new Foo("foo", "bar")));
